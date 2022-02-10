fd358835eaaf74712b43fc4e43560a83aec9ddc7
Alec N. Silva <ans@alec.pro.br>
2022-02-10 11:07:16 -0300

    Tarefas de 1 a 3 concluidas

---
+++ app/src/main/java/br/com/yt/android/chico_light_controller_view/lightContollerView.kt
@@ -0,0 +1,11 @@
+package br.com.yt.android.chico_light_controller_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class lightContollerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr)
